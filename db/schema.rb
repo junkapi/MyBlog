0f0a699e534ef6da0397d1ea21846d45bc2d9209
EC2 Default User <ec2-user@ip-172-31-27-199.us-east-2.compute.internal>
2019-01-27 12:17:33 +0000

    Add new_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_25_140032) do
+ActiveRecord::Schema.define(version: 2019_01_27_120425) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_01_25_140032) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "date"
   end
 
 end
